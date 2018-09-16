657bd238e5f21b0dd75ee1f7abf865cfea6472d5
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2018-09-16 13:27:02 +0200

    Add iverilog in the install_apio scripts

--- apio_template/install_apio.bat
+++ apio_template/install_apio.bat
@@ -1,4 +1,4 @@
 pip install apio tinyprog
-apio install system scons icestorm drivers
+apio install system scons icestorm iverilog drivers
 apio drivers --serial-enable
 @pause
--- apio_template/install_apio.sh
+++ apio_template/install_apio.sh
@@ -1,3 +1,3 @@
 pip install apio tinyprog
-apio install system scons icestorm
+apio install system scons icestorm iverilog
 apio drivers --serial-enable
